2222a08cc935f293e297b7c8742bb3ba218461c9
NikashSubedi2079 <nikash.subedi101@gmail.com>
2025-02-06 16:37:01 +0545

    button component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,17 +1,12 @@
+import Button from "./Button";
+
 function Home(){
   return(
-    <div>
-    <div>
-      Navbar section here
-    </div>
-    <div>
-      Hero section here
-    </div>
-    <div>
-      content section
-    </div>
-    <div>footer section</div>
-    </div>
+    <>
+   <Button/>
+   <Button/>    
+   </>
+    
   )
 }
 
