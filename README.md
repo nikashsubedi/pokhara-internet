16507e0441173464def5c38f4e5f6760b08064ff
NikashSubedi2079 <nikash.subedi101@gmail.com>
2025-02-06 11:57:29 +0545

    first commit

--- README.md
+++ README.md
@@ -49,3 +49,4 @@ export default tseslint.config({
 })
 ```
 "# pokhara-internet" 
+"# pokhara-internet" 
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+// import './App.css'
+import './index.css'
+
+
+function App() {
+  const name="nikash";
+
+  return (
+    <>
+    <h1>{name}</h1>
+    </>
+  )
+}
+
+export default App
